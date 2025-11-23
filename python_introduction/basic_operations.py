94ec1a708e618254bef681686b01644ec607881e
Sam-366 <ntlombe.s@gmail.com>
2025-11-23 13:45:03 +0200

    adding python3 to execute

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -14,3 +14,5 @@ product = number1 * number 2
 print(f"Addition of {number1} and {number2} is {sum_result}")
 print(f"Subtraction of {number1} and {number2} is {difference_result}")
 print(f"Multiplication of {number1} and {number2} is {product_result}")
+
+python3 basic_operations.py
