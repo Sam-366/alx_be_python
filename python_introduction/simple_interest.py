2eaaec26e8b934cefe68caec9263b92439695e41
Sam-366 <ntlombe.s@gmail.com>
2025-11-23 14:30:31 +0200

    formula edited and fixed

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -3,10 +3,9 @@
 principal = 1000
 rate = 0.05
 time = 3
-interest = i
 
 #define the formula
-i = p * r * t
+interest = principal * rate * time
 
-print(f"The simple interest is {i}")
+print(f"The simple interest is: {interest}")
 
