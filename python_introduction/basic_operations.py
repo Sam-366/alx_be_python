bc75635960303f6fe54dc081e1b1cc9eb2f39e12
Sam-366 <ntlombe.s@gmail.com>
2025-11-23 13:56:14 +0200

    variables fixed

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -7,12 +7,10 @@ sum = number1 + number2
 
 difference = number2 - number1
 
-product = number1 * number 2
+product = number1 * number2
 
 #print the results
 
-print(f"Addition of {number1} and {number2} is {sum_result}")
-print(f"Subtraction of {number1} and {number2} is {difference_result}")
-print(f"Multiplication of {number1} and {number2} is {product_result}")
-
-python3 basic_operations.py
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
